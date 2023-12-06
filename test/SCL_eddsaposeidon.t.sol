78c58a8748a975718e7bd0172d7717d79a1a3a61
rdubois-crypto <renaud.dubois.fr@gmail.com>
2023-12-06 18:28:16 +0100

    attempt to wrap poseidon bytecode

--- test/SCL_eddsaposeidon.t.sol
+++ test/SCL_eddsaposeidon.t.sol
@@ -16,16 +16,68 @@ pragma solidity >=0.8.19 <0.9.0;
 
 import "forge-std/Test.sol";
 
-uint256 constant _POSEIDONADD=0xcaca;
+address constant _POSEIDONADD=address(0xcaca);
 
-contract SCL_ed25519Test is Test {
+
+contract Poseidon5{
+  
+   //a pointer to the bytecode of the contract
+   address bytecode;
+
+   constructor(address where){
+    bytecode=where;
+   }
+
+   function poseidon(uint256[5] memory input) public returns(bool flag, bytes memory res)
+    {
+      bytes memory payload = abi.encodeWithSignature("poseidon(uint256[5])",input);
+
+     return( address(bytecode).call(payload));
+     
+    }
+}
+
+contract SCL_PoseidonTest is Test {
+
 
 function test_poseidon5() public{
    string memory deployData = vm.readFile("src/hash/poseidon5.json");
-   bytes memory bytecode = abi.decode(vm.parseJson(deployData, ".Bytecode"), (bytes));
-   vm.etch(address(uint160(_POSEIDONADD)), bytecode);    
+   bytes memory bytecode = abi.decode(vm.parseJson(deployData, ".poseidon5_bytecode"), (bytes));
+
+   console.log("Contract:\n");
+   console.logBytes(bytecode);
+
+   vm.etch(_POSEIDONADD, bytecode);    
    
-    
+   uint size=bytecode.length;
+   console.log("size=",size);
+
+   /* 
+   address somewhere;
+   console.log("\n code somewhere:");
+   assembly{
+     somewhere:=create(bytecode, 0, size)
+   }
+   console.logBytes(somewhere.code); 
+   */
+
+   uint256[5] memory hash_in=[uint256(1),2,0,0,0];
+
+   Poseidon5 hash=new Poseidon5(_POSEIDONADD);
+   //Poseidon5 hash=new Poseidon5(somewhere);
+  
+
+   console.log("Contract wrapper:\n");
+   console.logBytes(address(hash).code); 
+  
+    bytes memory res;
+
+    (,res)=hash.poseidon(hash_in);
+
+    console.log("returned:\n");
+    console.logBytes(res);
+
+
 }
 
 
