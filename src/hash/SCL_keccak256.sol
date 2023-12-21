6e6719cd2e39420b5f0907816b33959d359402a5
rdubois-crypto <renaud.dubois.fr@gmail.com>
2023-12-21 17:34:30 +0100

    begin solidity merkle

--- src/hash/SCL_keccak256.sol
+++ src/hash/SCL_keccak256.sol
@@ -7,8 +7,21 @@
 /*                                         |__/|_|           
 /*              
 /* Copyright (C) 2023 - Renaud Dubois - This file is part of SCL (Smooth CryptoLib) project
-/* License: This software is licensed under MIT License                                        
+/* License: This software is licensed under MIT License                        
 /********************************************************************************************/
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.19 <0.9.0;
+
+
+ /** from OZ repo https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/cryptography/MerkleProof.sol
+   * @dev Implementation of keccak256(abi.encode(a, b)) that doesn't allocate or expand memory.
+   */
+function _efficientHash(bytes32 a, bytes32 b) pure returns (bytes32 value) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            mstore(0x00, a)
+            mstore(0x20, b)
+            value := keccak256(0x00, 0x40)
+        }
+    }
 
