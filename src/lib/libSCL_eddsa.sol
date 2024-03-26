4347fb1d6c98f57bc38f6a53d9b56e078e8dd4f0
renaud dubois <renaud.dubois.fr@gmail.com>
2024-03-26 17:27:58 +0100

    implement ed25519 verification using the generic ecmulmuladd RIPB4

--- src/lib/libSCL_eddsa.sol
+++ src/lib/libSCL_eddsa.sol
@@ -13,30 +13,132 @@ pragma solidity >=0.8.19 <0.9.0;
 
 
 import "@solidity/hash/SCL_sha512.sol";
+
+import  "@solidity/modular/SCL_ModInv.sol"; 
+import "@solidity/fields/SCL_wei25519.sol";
+import "@solidity/elliptic/SCL_Isogeny.sol";
+
+import "@solidity/lib/libSCL_ripB4.sol";
+
+
 //5.1.5.  Key Generation
 
 
 //the name of the library 
 library SCL_EDDSA{
+ 
+ function ecPow128(uint256 X, uint256 Y, uint256 ZZ, uint256 ZZZ) public returns(uint256 x128, uint256 y128){
+   assembly{
+   function vecDbl(x, y, zz, zzz) -> _x, _y, _zz, _zzz{
+            let T1 := mulmod(2, y, p) //U = 2*Y1, y free
+                let T2 := mulmod(T1, T1, p) // V=U^2
+                let T3 := mulmod(x, T2, p) // S = X1*V
+                T1 := mulmod(T1, T2, p) // W=UV
+                let T4 := addmod(mulmod(3, mulmod(x,x,p),p),mulmod(a,mulmod(zz,zz,p),p),p)//M=3*X12+aZZ12  
+                _zzz := mulmod(T1, zzz, p) //zzz3=W*zzz1
+                _zz := mulmod(T2, zz, p) //zz3=V*ZZ1
+
+                _x := addmod(mulmod(T4, T4, p), mulmod(pMINUS_2, T3, p), p) //X3=M^2-2S
+                T2 := mulmod(T4, addmod(_x, sub(p, T3), p), p) //-M(S-X3)=M(X3-S)
+                _y := addmod(mulmod(T1, y, p), T2, p) //-Y3= W*Y1-M(S-X3), we replace Y by -Y to avoid a sub in ecAdd
+                _y:= sub(p, _y)
+         }
+         for {x128:=0} lt(x128, 128) { x128:=add(x128,1) }{
+           X, Y, ZZ, ZZZ := vecDbl(X, Y, ZZ, ZZZ)
+         }
+         }
+      ZZ=ModInv(ZZ, p);
+      ZZZ=ModInv(ZZZ,p);
+      x128=mulmod(X, ZZ, p);
+      y128=mulmod(Y, ZZZ, p);
+}
+
+ function BasePointMultiply(uint256 scalar) internal returns (uint256[2] memory R) {
+    uint256 [10] memory Q=[0,0,0,0, p, a, gx, gy, gpow2p128_x, gpow2p128_y ];////store Qx, Qy, Q'x, Q'y , p, a, gx, gy, gx2pow128, gy2pow128 
+    //abusing RIPB4 for base point multiplication
+    R=SCL_RIPB4.ecMulMulAdd_B4(Q, scalar, 0);
+
+ }
 
  function HashSecret(uint256 secret) public pure returns (uint256 expanded){
    uint64[16] memory buffer; 
-   secret=SCL_sha512.Swap256(secret);
-   uint256 a; 
+   
+ 
    uint256 low;
+   uint256 high;
 
    buffer[0]=uint64((secret>>192)&0xffffffffffffffff);
    buffer[1]=uint64((secret>>128)&0xffffffffffffffff);
    buffer[2]=uint64((secret>>64)&0xffffffffffffffff);
    buffer[3]=uint64(secret&0xffffffffffffffff);
-   buffer[4]=0x80;
+
+/*
+   buffer[0]=uint64((secret)&0xffffffffffffffff);
+   buffer[1]=uint64((secret>>64)&0xffffffffffffffff);
+   buffer[2]=uint64((secret>>128)&0xffffffffffffffff);
+   buffer[3]=uint64((secret>>192)&0xffffffffffffffff);
+*/
+
+   buffer[4]=uint64(0x80)<<56;
    buffer[15]=0x100;//length is 256 bits
 
-   (low,a)=SCL_sha512.SHA512(buffer);
-    a &= (1 << 254) - 8;
-    a |= (1 << 254);
+   (low,high)=SCL_sha512.SHA512(buffer);
+    expanded=low;
+    
+    expanded=SCL_sha512.Swap256(expanded);
+  expanded &= (1 << 254) - 8;
+   expanded |= (1 << 254);
 
-    return a;
+    return expanded;
+ }
+
+ //function exposed for RFC8032 compliance, but SetKey is more efficient
+ function ExpandSecret(uint256 secret) public returns (uint256[2] memory Kpub)
+ {
+   
+   secret=HashSecret(secret);
+ 
+   Kpub=BasePointMultiply(secret);
+   (Kpub[0], Kpub[1])=WeierStrass2Edwards(Kpub[0], Kpub[1]);
+
+ }
+
+ //eddsa benefit from the 255 bits to compress the parity of y in msb bit
+ function edCompress(uint256[2] memory Kpub) public returns(uint256 KPubC){
+  KPubC=Kpub[0] +((Kpub[1]&1)<<255) ;
+
+  return KPubC;
+ }
+
+ function edDecompress() internal returns (uint256[2] memory Kpub){
+
+ }
+ 
+ function SetKey(uint256 secret) public returns (uint256[2] memory Kpub)
+ {
+
+ }
+
+ //Rs is the 
+ function Verify(bytes memory msg, uint256 r, uint256 s, uint256[4] memory extKpub) public returns(bool flag){
+   uint256 A=edCompress(Kpub);
+   uint256 k;
+   uint64[16] memory tampon;
+   uint256[2] memory S;
+   //todo: add parameters checking
+   tampon=SCL_sha512.eddsa_sha512(r,A,msgo);
+   (S[0], S[1]) = SCL_sha512.SHA512(tampon);
+   k= SCL_sha512.Swap512(S); //endianness curse
+
+   uint256 [10] memory Q=[Q[0], Q[1],Q[2], Q[3], p, a, gx, gy, gpow2p128_x, gpow2p128_y ];
+
+   //3.  Check the group equation [8][S]B = [8]R + [8][k]A'.  It's sufficient, 
+   //but not required, to instead check [S]B = R + [k]A'.
+   //SCL tweak equality to substraction to check [S]B - [k]A' = [S]B + [n-k]A' = R 
+   S=SCL_RIPB4.ecMulMulAdd_B4(Q, s, n-k);
+   
+   return(S[0]==r);    
+
  }
 
 }
--- test/libSCL_eddsa.t.sol
+++ test/libSCL_eddsa.t.sol
@@ -21,14 +21,18 @@ import  "@solidity/lib/libSCL_eddsa.sol";
 contract Test_eddsa is Test {
 
  
-    function test_SHA512_ed255KG()  public view{
+    function test_SHA512_ed255KG()  public {
         //vector 3 input secret key
         uint256 secret=0xc5aa8df43f9f837bedb7442f31dcb7b166d38535076f094b85ce3a2e0b4458f7;
-      
+        //uint256 secret=0xf758440b2e3ace854b096f073585d366b1b7dc312f44b7ed7b839f3ff48daac5;
+
         uint256 res=SCL_EDDSA.HashSecret(secret);
         //expected hashed secret (obtained via sage):0x9ca91e9981a125131bf5c2c54e7f4dba113dc2155ba523908402d95e758b9a90
+        //909A8B755ED902849023A55B15C23D11BA4D7F4EC5C2F51B1325A181991EA99C
+        //6608C8666B9CDE2325F539D7D83386FE8187C6BE61D8A70C247190D64EDF5F1E
         console.log("res=%x",res);
-
+        uint256[2] memory Kpub=SCL_EDDSA.ExpandSecret(secret);
+         console.log("Kpub=%d %d",Kpub[0], Kpub[1]);
         //expected expanded
         //uint256 expanded=41911590414521875233341115108072091496810396974354451206977851026743843592848;
     }
