cdebc631ffa6c10ce181c12ef7b225121c83037c
renaud dubois <renaud.dubois.fr@gmail.com>
2024-05-24 15:58:09 +0200

    add Ed25519 Keygeneration

--- src/lib/libSCL_RIP6565.sol
+++ src/lib/libSCL_RIP6565.sol
@@ -16,6 +16,8 @@
 pragma solidity >=0.8.19 <0.9.0;
 
 import { delta, A, c, a,b,p, gx, gy, gpow2p128_x, gpow2p128_y} from "../fields/SCL_wei25519.sol";
+import "../modular/SCL_sqrtMod_5mod8.sol";
+
 
 //import modular inversion over prime field defined over curve subgroup of prime order
 import { ModInv } from "../modular/SCL_modular.sol"; 
@@ -24,9 +26,18 @@ import {ec_isOnCurve} from "../elliptic/SCL_ecOncurve.sol";
 //import point double multiplication and accumulation (RIP7696)
 import "../elliptic/SCL_mulmuladdX_fullgenW.sol";
 
-
+import "../../external/sha512/Sha2Ext.sol";
+import "../hash/SCL_sha512.sol";
 
 library SCL_RIP6565{
+
+
+
+    function Swap64(uint64 w) internal pure returns (uint64 x){
+     uint64 tmp= (w >> 32) | (w << 32);
+	 tmp = ((tmp & 0xff00ff00ff00ff00) >> 8) |    ((tmp & 0x00ff00ff00ff00ff) << 8); 
+	 x = ((tmp & 0xffff0000ffff0000) >> 16) |   ((tmp & 0x0000ffff0000ffff) << 16); 
+    }
 
 
 function Edwards2WeierStrass(uint256 x,uint256 y)  internal view returns (uint256 X, uint256 Y){
@@ -53,8 +64,16 @@ function WeierStrass2Edwards(uint256 X,uint256 Y)  internal view returns (uint25
 
 }
 
- 
+
  function BasePointMultiply(uint256 scalar) public view returns (uint256 x, uint256 y) {
+    uint256[6] memory Q=[gpow2p128_x,gpow2p128_y,p,a,gx,gy];
+ 
+    //abusing RIP7696 first opcode for base point multiplication
+    (x,y)=ecGenMulmuladdB4W(Q, scalar, 0);
+ }
+
+ //todo: speedup by splitting scalars
+ function BasePointMultiply_Edwards(uint256 scalar) public view returns (uint256 x, uint256 y) {
     uint256[6] memory Q=[gpow2p128_x,gpow2p128_y,p,a,gx,gy];
  
     //abusing RIP7696 first opcode for base point multiplication
@@ -68,5 +87,75 @@ function WeierStrass2Edwards(uint256 X,uint256 Y)  internal view returns (uint25
 
  }
 
+ 
+/* reduce a 512 bit number modulo curve order*/
+function Red512Modq(uint256[2] memory val) internal pure returns (uint256 h)
+{
 
+  return addmod(mulmod(val[0],
+  0xffffffffffffffffffffffffffffffec6ef5bf4737dcf70d6ec31748d98951d, 
+  0x1000000000000000000000000000000014def9dea2f79cd65812631a5cf5d3ed)
+                ,val[1],0x1000000000000000000000000000000014def9dea2f79cd65812631a5cf5d3ed);
+
+}
+
+ //eddsa benefit from the 255 bits to compress the parity of y in msb bit
+ function edCompress(uint256[2] memory Kpub) public pure returns(uint256 KPubC){
+  KPubC=Kpub[1] +((Kpub[0]&1)<<255) ;
+
+  return KPubC;
+ }
+ 
+ 
+
+ //function exposed for RFC8032 compliance (Edwards form), but SetKey is more efficient 
+ //(keep Weierstrass compatible with 7696)
+ function ExpandSecret(uint256 secret) public view returns (uint256 KpubC,uint256 expanded)
+ {
+  uint256[2] memory Kpub;
+
+   bytes memory input=abi.encodePacked(secret);
+   bytes32 high;
+   bytes32 low;
+
+   (high, low)=Sha2Ext.sha512(input);
+   
+   expanded=SCL_sha512.Swap256(uint256(high));
+   expanded &= (1 << 254) - 8;
+   expanded |= (1 << 254);
+
+ 
+   (Kpub[0], Kpub[1])=BasePointMultiply_Edwards(expanded);
+   KpubC=SCL_sha512.Swap256(edCompress(Kpub));//evil Bernstein loves swaps
+
+ }
+
+ 
+ //input are expressed msb first, as any healthy mind should.
+ function Verify(bytes memory msg, uint256 r, uint256 s, uint256[5] memory extKpub) public returns(bool flag){
+   uint256 [2] memory S;
+   uint256 A=extKpub[4];
+   uint256 k;
+   uint64[16] memory tampon;
+   
+   //todo: add parameters checking
+   //tampon=SCL_sha512.eddsa_sha512(r,A,msg);
+   //(S[0], S[1]) = SCL_sha512.SHA512(tampon);
+   //k= SCL_EDDSA.Red512Modq(SCL_sha512.Swap512(S)); //swap then reduce mod q
+   
+   //uint256 [10] memory Q=[extKpub[0], extKpub[1],extKpub[2], extKpub[3], p, a, gx, gy, gpow2p128_x, gpow2p128_y ];
+   
+   (S[0], S[1])=WeierStrass2Edwards(extKpub[0], extKpub[1]);
+  
+   //3.  Check the group equation [8][S]B = [8]R + [8][k]A'.  It's sufficient, 
+   //but not required, to instead check [S]B = R + [k]A'.
+   //SCL tweak equality to substraction to check [S]B - [k]A' = [S]B + [n-k]A' = R 
+   //S=SCL_RIPB4.ecMulMulAdd_B4(Q, s, n-k);
+   //(S[0], S[1])=WeierStrass2Edwards(S[0], S[1]);//back to edwards form
+   //uint256 recomputed_r=edCompress(S);
+   
+   //return(recomputed_r==r);    
+
+ }
+ 
 }
--- test/libSCL_rip6565.t.sol
+++ test/libSCL_rip6565.t.sol
@@ -33,13 +33,45 @@ contract SCL_Ed25519Test is Test {
   uint256 resX;
 
 
-  (resX,)=SCL_RIP6565.BasePointMultiply(2);
+  (resX,)=SCL_RIP6565.BasePointMultiply_Edwards(2);
   assertEq(resX, 0x36ab384c9f5a046c3d043b7d1833e7ac080d8e4515d7a45f83c5a14e2843ce0e);//expected 2G result
   
-  (resX,)=SCL_RIP6565.BasePointMultiply(5);
+  (resX,)=SCL_RIP6565.BasePointMultiply_Edwards(5);
    assertEq(resX, 0x49fda73eade3587bfcef7cf7d12da5de5c2819f93e1be1a591409cc0322ef233);//expected 5G result
  
  }
+
+ function test_expandSecret()public view {
+    uint256 KpubC;
+    uint256 expSec;
+    //vector 1 from rfc8032
+    uint256 secret1=0x4ccd089b28ff96da9db6c346ec114e0f5b8a319f35aba624da8cf6ed4fb8a6fb;
+    uint256 expected1=0x3d4017c3e843895a92b70aa74d1b7ebc9c982ccf2ec4968cc0cd55f12af4660c;
+
+    (KpubC,expSec)=SCL_RIP6565.ExpandSecret(secret1);
+    assertEq(KpubC, expected1);//expected public key
+ 
+    //vector 2 from rfc8032
+   uint256 secret2=0x3d4017c3e843895a92b70aa74d1b7ebc9c982ccf2ec4968cc0cd55f12af4660c;
+   
+
+    //vector 3 input secret key, lsb first
+    uint256 secret3=0xc5aa8df43f9f837bedb7442f31dcb7b166d38535076f094b85ce3a2e0b4458f7;
+    //expected public key, lsb fist
+    uint256 expected3=0xfc51cd8e6218a1a38da47ed00230f0580816ed13ba3303ac5deb911548908025;
+
+    (KpubC,expSec)=SCL_RIP6565.ExpandSecret(secret3);
+    assertEq(KpubC, expected3);//expected public key       
+ }
+
+
+ function test_Fuzz_ed255sqrtmod2(uint256 x) public {
+        uint256 val = mulmod(x, x, p);
+        uint256 rac = SqrtMod(val);
+       
+        assertEq(mulmod(rac, rac, p), val);
+    }
+
 
 
 }
