57985cac09e824b204758ad64fdd43108aff667a
renaud dubois <renaud.dubois.fr@gmail.com>
2024-05-24 15:58:09 +0200

    add Ed25519 Keygeneration

---
+++ src/hash/SCL_sha512.sol
@@ -0,0 +1,57 @@
+/********************************************************************************************/
+/*
+/*     ___                _   _       ___               _         _    _ _    
+/*    / __|_ __  ___  ___| |_| |_    / __|_ _ _  _ _ __| |_ ___  | |  (_) |__ 
+/*    \__ \ '  \/ _ \/ _ \  _| ' \  | (__| '_| || | '_ \  _/ _ \ | |__| | '_ \
+/*   |___/_|_|_\___/\___/\__|_||_|  \___|_|  \_, | .__/\__\___/ |____|_|_.__/
+/*                                         |__/|_|           
+/*              
+/* Copyright (C) 2023 - Renaud Dubois - This file is part of SCL (Smooth CryptoLib) project
+/* License: This software is licensed under MIT License                                        
+/* 
+/********************************************************************************************/
+
+// SPDX-License-Identifier: MIT
+
+//https://emn178.github.io/online-tools/sha512.html
+
+pragma solidity >=0.8.19 <0.9.0;
+
+import "../../external/sha512/Sha2Ext.sol";
+
+library SCL_sha512 {
+ 
+    function Swap512(uint256[2] memory w) internal pure returns(uint256[2] memory r)
+    {
+        r[0]=Swap256(w[1]);
+        r[1]=Swap256(w[0]);
+
+        return r;
+    }
+
+    function Swap256(uint256 w) internal pure returns (uint256 x)
+    {
+        return uint256(Swap128(uint128(w>>128)))^(uint256(Swap128(uint128(w&0xffffffffffffffffffffffffffffffff)))<<128);
+    }
+
+    function Swap128(uint128 w) internal pure returns (uint128 x)
+    {
+        return uint128(Swap64(uint64(w>>64)))^(uint128(Swap64(uint64(w&0xffffffffffffffff)))<<64);
+    }
+
+
+    function Swap64(uint64 w) internal pure returns (uint64 x){
+     uint64 tmp= (w >> 32) | (w << 32);
+	 tmp = ((tmp & 0xff00ff00ff00ff00) >> 8) |    ((tmp & 0x00ff00ff00ff00ff) << 8); 
+	 x = ((tmp & 0xffff0000ffff0000) >> 16) |   ((tmp & 0x0000ffff0000ffff) << 16); 
+    }
+
+    //Hash 256 bit input as LE
+    function HashLE(uint256 secret) public pure returns (uint256 h, uint256 l){
+      bytes32 low;
+      bytes32 high;
+      (high, low)= Sha2Ext.sha512(bytes(abi.encodePacked(secret)));
+
+      return(uint256(high), uint256(low));
+    }
+}
--- test/libSCL_external_sha512.t.sol
+++ test/libSCL_external_sha512.t.sol
@@ -17,6 +17,7 @@ import {stdJson} from "forge-std/StdJson.sol";
 /* import rip7212 */
 
 import "../external/sha512/Sha2Ext.sol";
+import "../src/hash/SCL_sha512.sol";
 
 contract SCL_Ed25519Test is Test {
 
@@ -27,20 +28,21 @@ contract SCL_Ed25519Test is Test {
 
     (high, low)=Sha2Ext.sha512("abc");
     //https://asecuritysite.com/encryption/md5?word=abc
-    //console.log("%x %x",uint256(high), uint256(low));
     assertEq(high, 0xddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a);
     assertEq(low, 0x2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f);
 
-    (high, low)=Sha2Ext.sha512(hex"616263");//equivalent vector input
+   (high, low)=Sha2Ext.sha512(hex"616263");//equivalent vector input
 
     assertEq(high, 0xddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a);
     assertEq(low, 0x2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f);
     
+    
     //https://www.di-mgt.com.au/sha_testvectors.html
-    (high, low)=Sha2Ext.sha512("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu");
+   (high, low)=Sha2Ext.sha512("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu");
    
     assertEq(high,0x8e959b75dae313da8cf4f72814fc143f8f7779c6eb9f7fa17299aeadb6889018 );
     assertEq(low,0x501d289e4900f7e4331b99dec4b5433ac7d329eeb6dd26545e96e55b874be909);
+  
 
  }
 
